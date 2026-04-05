2cf5fdb5133cbfcc130ac205981355b631e46c65
Guiznx <guizinfut02@gmail.com>
2026-04-05 12:29:54 -0400

    api/upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,18 +1,17 @@
-let scripts = {};
-
 export default function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).end();
   }
 
+  globalThis.scripts = globalThis.scripts || {};
+
   let { code } = req.body;
 
-  let id = Date.now();
+  let id = Date.now().toString();
 
-  // mini ofuscação
   let encoded = Buffer.from(code).toString("base64");
 
-  scripts[id] = encoded;
+  globalThis.scripts[id] = encoded;
 
   let link = `https://${req.headers.host}/api/raw?id=${id}`;
 
